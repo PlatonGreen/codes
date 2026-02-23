cb23e5f22ab3429ff3fcdc28f5c249c4aa5793d7
Platon <185079474+PlatonGreen@users.noreply.github.com>
2026-02-23 19:22:00 +0100

    Create 05.c
    Продолжите программу, которая должна вывести все объявленные переменные (в порядке их объявления) каждую с новой строки. При этом никаких пробелов в выводе быть не должно. Воспользуйтесь для такого форматированного вывода функцией printf(). Символьная переменная должна быть выведена как символ, вещественная без ограничения точности.

---
+++ C/05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char byte = 65;
+    unsigned short height = 1055;
+    int distance = 1000000;
+    float pi = 3.1415f; 
+    printf("%c\n", byte);
+    printf("%u\n", height);
+    printf("%d\n", distance);
+    printf("%f\n", pi);
+    return 0;
+}
