81cbccd10b030b2ecf3609e0217d98d79942b3b1
Platon <185079474+PlatonGreen@users.noreply.github.com>
2026-02-23 19:38:49 +0100

    Во входном потоке stdin находятся не менее 10 вещественных чисел со значениями температуры по дням, записанных в одну строчку через точку с запятой. Необходимо прочитать первые 10 чисел и вывести в выходной поток stdout последние пять (из прочитанных) в одну строчку через пробел с точностью до десятых.

---
+++ C/16.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+    scanf("%f; %f; %f; %f; %f; %f; %f; %f; %f; %f", &a1, &a2, &a3, &a4, &a5, &a6, &a7, &a8, &a9, &a10);
+    printf("%0.1f %0.1f %0.1f %0.1f %0.1f", a6, a7, a8, a9, a10);
+   
+}
