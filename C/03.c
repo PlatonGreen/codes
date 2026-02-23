0c9577e111c17a16ce3d76fda98d8d6f5b90592e
Platon <185079474+PlatonGreen@users.noreply.github.com>
2026-02-23 19:17:36 +0100

    Create 03.c

---
+++ C/03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b ,c;
+    a=getchar();
+    b=getchar();
+    c=getchar();
+    putchar(c);
+    putchar(b);
+    putchar(a);
+
+    return 0;
+}
